9e297301f3f7301436b6a44561cb0262bfaf3588
batibot323 <hani.herbert.ho@gmail.com>
2020-02-05 14:37:26 +0800

    Added two more inputs to get the three points we need to compute for perimeter and area of a triangle.

--- src/app/text-box/text-box.component.ts
+++ src/app/text-box/text-box.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TextBoxComponent implements OnInit {
   private point1: { x: number, y: number };
   private point2: { x: number, y: number };
+  private point3: { x: number, y: number };
 
   constructor() { }
 
@@ -17,8 +18,10 @@ export class TextBoxComponent implements OnInit {
   passPoints(...value: string[]) {
     this.point1 = { x: Number.parseInt(value[0]), y: Number.parseInt(value[1]) };
     this.point2 = { x: Number.parseInt(value[2]), y: Number.parseInt(value[3]) };
+    this.point3 = { x: Number.parseInt(value[4]), y: Number.parseInt(value[5]) };
     console.log(this.point1);
     console.log(this.point2);
+    console.log(this.point3);
   }
 
 }
