bfaeb247e518fa0adc246cc163635bc84c7dc3d2
batibot323 <hani.herbert.ho@gmail.com>
2020-02-05 14:13:48 +0800

    Generated text-box.

---
+++ src/app/text-box/text-box.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TextBoxComponent } from './text-box.component';
+
+
+
+@NgModule({
+  declarations: [TextBoxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TextBoxModule { }
