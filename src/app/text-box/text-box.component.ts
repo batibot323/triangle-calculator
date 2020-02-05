513ff4f4b8ca28da69773c338fb2a90bda0e6e8f
batibot323 <hani.herbert.ho@gmail.com>
2020-02-05 14:33:30 +0800

    Added four input text boxes to get coordinates of two points that will form a line.

--- src/app/text-box/text-box.component.ts
+++ src/app/text-box/text-box.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-box.component.css']
 })
 export class TextBoxComponent implements OnInit {
+  private point1: { x: number, y: number };
+  private point2: { x: number, y: number };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  passPoints(...value: string[]) {
+    this.point1 = { x: Number.parseInt(value[0]), y: Number.parseInt(value[1]) };
+    this.point2 = { x: Number.parseInt(value[2]), y: Number.parseInt(value[3]) };
+    console.log(this.point1);
+    console.log(this.point2);
   }
 
 }
